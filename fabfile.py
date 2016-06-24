0caf1dc6e01ea6031022a09847a8444632adcf13
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2016-06-24 16:38:48 +0530

    Added migrate, rebuild, collect-static, deploy, rsync fab commands

--- fabfile.py
+++ fabfile.py
@@ -13,6 +13,7 @@ full_path = os.path.realpath(__file__).split('fabfile.py')[0] + 'config.yaml'
 config = yaml.load(open(full_path))
 data = config.get("local") or {}
 run_on = "local"
+env.hosts = ['localhost']
 
 
 def run_local():
@@ -72,3 +73,78 @@ def activate_env_install_requirements():
         print("Please check the '%s' env and requirements file "
               "paths in the config file." % str(run_on))
 
+
+def manage_py(command):
+    function = get_function()
+    if not function:
+        return
+
+    if data and data.get("env_path") and data.get("repository_root"):
+        with cd(data.get("repository_root")):
+            function(
+                "/bin/bash -l -c 'source %s/bin/activate && " % (
+                    data.get("env_path")
+                ) + "python %s/manage.py %s'" % (
+                    data.get("repository_root"), command)
+            )
+    else:
+        print("Please check the '%s' env_path and repository_root "
+              "in the config file." % str(run_on))
+
+
+def migrate():
+    manage_py("migrate")
+
+
+def collect_static():
+    manage_py("collectstatic")
+
+
+def rebuild_index():
+    manage_py('rebuild_index --noinput')
+
+
+def restart_celery():
+    function = get_function()
+    if not function:
+        return
+
+    run('supervisorctl restart celeryd')
+
+
+def deploy():
+    activate_env_install_requirements()
+    migrate()
+    # collect_static()
+
+
+def rsync_with_settings():
+    if (
+        config.get("local") and config.get("local").get("repository_root") and
+        data and data.get("repository_root")
+    ):
+        rsync_project(
+            local_dir=config.get("local").get("repository_root") + '/',
+            remote_dir=data.get("repository_root") + '/',
+            exclude=['.git', 'db.sqlite3', 'settings_local.py',
+                     '.pyc', '__pycache__', 'CACHE']
+        )
+    else:
+        print("Please check the 'local' and '%s' repository_root paths "
+              "in the config file." % str(run_on))
+
+
+def rsync_without_settings():
+    if (
+        config.get("local") and config.get("local").get("repository_root") and
+        data and data.get("repository_root")
+    ):
+        rsync_project(
+            local_dir=config.get("local").get("repository_root") + '/',
+            remote_dir=data.get("repository_root") + '/',
+            exclude=['.git', 'db.sqlite3', 'settings.py', 'settings_local.py',
+                     '.pyc', '__pycache__', 'CACHE']
+        )
+    else:
+        print("Please check the 'local' and '%s' repository_root paths "
+              "in the config file." % str(run_on))
